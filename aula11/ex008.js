9544e2f0f63b1961b4a3e8388c2432e2b9cc6fc0
João Bizzo <92961230+joaobizzo@users.noreply.github.com>
2023-02-24 15:53:21 -0300

    Delete aula11 directory

--- aula11/ex008.js
+++
@@ -1,7 +0,0 @@
-var vel = 60.5
-console.log(`A velocidade do seu carro é ${vel}Km/h`)
-
-if(vel > 60){
-    console.log(`MULTADO`)
-}
-console.log(`Dirija com cuidado`)
--- aula11/ex009.js
+++
@@ -1,7 +0,0 @@
-var pais = 'EUA'
-console.log(`Vivendo em ${pais}`)
-if(pais != 'Brasil'){
-    console.log('Você é estrangeiro')
-} else{
-    console.log('Brasileiro')
-}
